2e660a0c96c4d756535a52850ee223d871b81e7f
NoahShin <noahshin11@gmail.com>
2023-11-28 20:23:27 +0900

    using impl and self as parameter

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,34 @@ struct Person {
   last_name: String,
   age: u32,
 }
+
+impl Person {
+  fn some_function() {
+    println!("somefuncstion");
+  }
+
+  fn display_age(&self) {
+    println!("Current Age: {}", self.age);
+  }
+}
 fn main() {
+
+  Person::some_function();
+
   let person = Person {
     name: "Noah".to_string(),
     last_name: "Shin".to_string(),
     age: 30,
   };
+
+  let person2 = Person {
+    name: "Mow".to_string(),
+    last_name: "Yang".to_string(),
+    age: 42,
+  };
+
+  person.display_age();
+  person2.display_age();
 
   println!("{}", person.name);
   println!("{}", person.last_name);
