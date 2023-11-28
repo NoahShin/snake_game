3571d2d00f2bc0862b2ad3a19d3e2eb397cf4445
NoahShin <noahshin11@gmail.com>
2023-11-28 20:37:18 +0900

    [derive(Debug)]

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,54 @@
+#[derive(Debug)]
+enum PersonId {
+  Passport,
+  IdentityCard,
+}
 struct Person {
   name: String,
   last_name: String,
   age: u32,
+  id: PersonId,
 }
 
 impl Person {
-  fn some_function() {
-    println!("somefuncstion");
+  fn new11() -> Person {
+    Person {
+      name: "Default".to_string(),
+      last_name: "Default".to_string(),
+      age: 0,
+      id: PersonId::IdentityCard
+    }
   }
-
-  fn display_age(&self) {
-    println!("Current Age: {}", self.age);
+  fn change_age(&mut self, new_age: u32) {
+    self.age = new_age;
   }
 }
 fn main() {
+  let mut aa = Person::new11();
+  aa.change_age(42);
+  println!("{} {} {}", aa.age, aa.name, aa.last_name);
 
-  Person::some_function();
-
-  let person = Person {
+  let mut person = Person {
     name: "Noah".to_string(),
     last_name: "Shin".to_string(),
     age: 30,
+    id: PersonId::Passport,
   };
 
-  let person2 = Person {
+  let mut person2 = Person {
     name: "Mow".to_string(),
     last_name: "Yang".to_string(),
     age: 42,
+    id: PersonId::IdentityCard,
   };
 
-  person.display_age();
-  person2.display_age();
+  person.change_age(22);
+  person2.change_age(21);
 
-  println!("{}", person.name);
-  println!("{}", person.last_name);
+  
   println!("{}", person.age);
+  println!("{}", person2.age);
+  println!("{:?}", person2.id);
 
 }
 
