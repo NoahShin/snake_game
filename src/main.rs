9ba428d98b861ab712b338949d8f0774315ab54e
NoahShin <noahshin11@gmail.com>
2023-11-28 21:23:39 +0900

    module화 까지

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,12 @@
-#[derive(Debug)]
-enum PersonId {
-  Passport,
-  IdentityCard,
-}
-struct Person {
-  name: String,
-  last_name: String,
-  age: u32,
-  id: PersonId,
-}
+use snake_game::Person;
+use snake_game::Animal;
+use snake_game::log_info;
+use snake_game::log_info_2;
 
-impl Person {
-  fn new11() -> Person {
-    Person {
-      name: "Default".to_string(),
-      last_name: "Default".to_string(),
-      age: 0,
-      id: PersonId::IdentityCard
-    }
-  }
-  fn change_age(&mut self, new_age: u32) {
-    self.age = new_age;
-  }
-}
+
 fn main() {
-  let mut aa = Person::new11();
-  aa.change_age(42);
-  println!("{} {} {}", aa.age, aa.name, aa.last_name);
-
-  let mut person = Person {
-    name: "Noah".to_string(),
-    last_name: "Shin".to_string(),
-    age: 30,
-    id: PersonId::Passport,
-  };
-
-  let mut person2 = Person {
-    name: "Mow".to_string(),
-    last_name: "Yang".to_string(),
-    age: 42,
-    id: PersonId::IdentityCard,
-  };
-
-  person.change_age(22);
-  person2.change_age(21);
-
-  
-  println!("{}", person.age);
-  println!("{}", person2.age);
-  println!("{:?}", person2.id);
-
+  let mut person = Person::new();
+  let animal = Animal(String::from("dog"));
+  log_info(person);
+  log_info_2(&animal);
 }
-
